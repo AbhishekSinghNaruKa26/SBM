38c16299bfae77019a72d54bd9322bcf56c911e7
Abhishek Singh Naruka <abhisheksingh200326@gmail.com>
2025-09-27 11:57:55 +0530

    admin Updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,26 @@
-
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import Header from './Component/Header';
 
 function App() {
 
 
   return (
     <>
+    <Router>
+
+      <Routes>
+
+        <Route path='/' element={
+          <>
+          <Header/>
+          </>
+        }>
+        </Route>
+
+
+      </Routes>
+
+    </Router>
      <div className="bg-orange-500">Jay Shree Ram</div>
     </>
   )
