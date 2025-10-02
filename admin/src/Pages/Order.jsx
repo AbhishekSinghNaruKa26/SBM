810ffc81b30772c8534a07245e1e4729b564e086
amar7773 <amarsingh17579@gmail.com>
2025-10-02 18:10:02 +0530

    new changes hai bhai

--- admin/src/Pages/Order.jsx
+++ admin/src/Pages/Order.jsx
@@ -4,7 +4,7 @@ import Details from "../Component/Details";
 import orde from "../Data/adminorder";
 function Order() {
   return (
-    <div className="shadow-xl rounded-xl mt-5 max-w-[1500px] w-full mx-auto">
+    <div className="shadow-md rounded-md  max-w-[1500px] w-full mx-auto bg-pink-200">
       <OrderHeader />
       <Details orde={orde} />
     </div>
