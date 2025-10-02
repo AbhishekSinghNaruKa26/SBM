506c0c9b82ba968b0961e876a40643b422f705f0
amar7773 <amarsingh17579@gmail.com>
2025-10-02 18:48:09 +0530

    new update in header

--- admin/src/Component/Header.jsx
+++ admin/src/Component/Header.jsx
@@ -10,97 +10,89 @@ import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogout } from "react-icons/md";
 import { useState } from "react";
 
+const Header = ({ selected, setSelected }) => {
+  const [open, setOpen] = useState(false);
 
-
-const Header = ({selected,setSelected}) => {
-
-  const [open , setOpen] = useState(false);
-
-
- const allButtons = [
-    {id:1, icon:<HiOutlineChartBar/> , name:"Dashboard" },
-    {id:2, icon:< PiBasket/> , name:"Orders" },
-    {id:3, icon:<BsHandbag/> , name:"Products" },
-    {id:4, icon:<BsFilePlus/> , name:"Add Products" },
-    {id:5, icon:<HiOutlineUsers/> ,name:"Users" },
-    {id:6, icon:<CgProfile/> , name:"My Profile" },   
-    {id:7, icon:<MdOutlineLogout/> , name:"Logout" },
-  ]
-
+  const allButtons = [
+    { id: 1, icon: <HiOutlineChartBar />, name: "Dashboard" },
+    { id: 2, icon: <PiBasket />, name: "Orders" },
+    { id: 3, icon: <BsHandbag />, name: "Products" },
+    { id: 4, icon: <BsFilePlus />, name: "AddProducts" },
+    { id: 5, icon: <HiOutlineUsers />, name: "Users" },
+    { id: 6, icon: <CgProfile />, name: "My Profile" },
+    { id: 7, icon: <MdOutlineLogout />, name: "Logout" },
+  ];
 
   return (
     <>
-
-    {/* Header */}
-    <div className="p-3 py-4 md:p-4 bg-red-500 text-white flex justify-between items-center border-b-2 fixed top-0 w-full">
-
-      {/* Logo */}
-      <div className="flex items-center gap-3 font-semibold bg-black p-1 rounded"><img className="max-w-[70px]" src="SMB-LOGO.png" alt="" /></div>
-
-      {/* bars */}
-      <div onClick={()=>setOpen(!open)} className="text-[25px] md:hidden"><HiBars3 /></div>
-
-    </div>
-
-    {/* If bars clicked */}
-    {open &&
-    <>
-    <div className="bg-red-500 max-w-[350px] h-screen fixed w-full  top-0 left-0 text-white p-3">
-
-      {/* Logo and Cross */}
-      <div className="flex justify-between border-b  pb-3">
-
+      {/* Header */}
+      <div className="p-3 py-4 md:p-4 bg-red-500 text-white flex justify-between items-center border-b-2 fixed top-0 w-full">
         {/* Logo */}
-        <div className="flex items-center gap-3 font-semibold bg-black p-1 rounded"><img className="max-w-[70px]" src="SMB-LOGO.png" alt="" /></div>
-
-        {/* Cross */}
-        <div onClick={()=>setOpen(!open)} className="text-2xl"><IoCloseSharp /></div>
-      </div>
-      
-
-      {/* Profile Details */}
-      <div className="mt-8 flex rounded-2xl items-center gap-4 shadow-2xl py-3">
-
-        {/* Image */}
-        <div>
-          <img className="max-w-[65px] rounded-full border" src="self.jpeg" alt="" />
+        <div className="flex items-center gap-3 font-semibold bg-black p-1 rounded">
+          <img className="max-w-[70px]" src="SMB-LOGO.png" alt="" />
         </div>
 
-        {/* Name And Email */}
-        <div>
-          <div className="font-bold tracking-wider">Abhishek Singh</div>
-          <div className="text-sm">abhisheknaruka7773@gmail.com</div>
+        {/* bars */}
+        <div onClick={() => setOpen(!open)} className="text-[25px] md:hidden">
+          <HiBars3 />
         </div>
-
       </div>
 
-      {/* All Buttons of panel */}
-      <div className="p-2 mt-10 space-y-7 font-semibold">
+      {/* If bars clicked */}
+      {open && (
+        <>
+          <div className="bg-red-500 max-w-[350px] h-screen fixed w-full  top-0 left-0 text-white p-3">
+            {/* Logo and Cross */}
+            <div className="flex justify-between border-b  pb-3">
+              {/* Logo */}
+              <div className="flex items-center gap-3 font-semibold bg-black p-1 rounded">
+                <img className="max-w-[70px]" src="SMB-LOGO.png" alt="" />
+              </div>
 
-        {allButtons.map((item,index)=>(
-        <div onClick={()=>setSelected(item.name)} key={index} className={`flex items-center gap-2 rounded p-2 text-xl ${selected===item.name ? "bg-gray-200 text-red-600" :  ""}`}>
+              {/* Cross */}
+              <div onClick={() => setOpen(!open)} className="text-2xl">
+                <IoCloseSharp />
+              </div>
+            </div>
 
-        <div>{item.icon}</div>
-        <div>{item.name}</div>
+            {/* Profile Details */}
+            <div className="mt-8 flex rounded-2xl items-center gap-4 shadow-2xl py-3">
+              {/* Image */}
+              <div>
+                <img
+                  className="max-w-[65px] rounded-full border"
+                  src="self.jpeg"
+                  alt=""
+                />
+              </div>
 
-      </div>
-        ))
+              {/* Name And Email */}
+              <div>
+                <div className="font-bold tracking-wider">Abhishek Singh</div>
+                <div className="text-sm">abhisheknaruka7773@gmail.com</div>
+              </div>
+            </div>
 
-        }
-      
-      
-      </div>
-      
-
-    </div>
+            {/* All Buttons of panel */}
+            <div className="p-2 mt-10 space-y-7 font-semibold">
+              {allButtons.map((item, index) => (
+                <div
+                  onClick={() => setSelected(item.name)}
+                  key={index}
+                  className={`flex items-center gap-2 rounded p-2 text-xl ${
+                    selected === item.name ? "bg-gray-200 text-red-600" : ""
+                  }`}
+                >
+                  <div>{item.icon}</div>
+                  <div>{item.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
+  );
+};
 
-    }
-
-  
-    </>
-
-  )
-}
-
-export default Header
+export default Header;
